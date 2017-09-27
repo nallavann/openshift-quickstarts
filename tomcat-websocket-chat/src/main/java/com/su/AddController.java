2e0edfb714370f4290ceef87072f88bad17bc1af
Sudharsanan <Sudharsanan@192.168.99.1>
2017-09-27 22:46:32 +0530

    controller commit

---
+++ tomcat-websocket-chat/src/main/java/com/su/AddController.java
@@ -0,0 +1,24 @@
+package com.su;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+
+		@RequestMapping(value="/add", method = RequestMethod.GET)
+		public ModelAndView addMe(@RequestParam String a1, @RequestParam String a2) {
+			//System.out.println(a1);
+			//System.out.println(a2);
+			int i = Integer.parseInt(a1);
+			int j = Integer.parseInt(a2);
+			int k = i + j;
+			
+			ModelAndView result = new ModelAndView("display");
+			result.addObject("result",k);
+			return result;
+		}
+}
